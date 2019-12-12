4c4ee045feb6756c964a189afa97543a59fa5204
scherzoq <johntaichin@gmail.com>
2019-12-12 17:20:00 -0500

    Basic Javascript Projects
    Basic Javascript Projects

---
+++ Assignments and Challenges/JS/challenge.js
@@ -0,0 +1,5 @@
+var a = "This is a string assigned to an variable";
+window.alert(a);
+
+var b = "This is a concatenated string " + "assigned to a variable";
+document.write(b);
---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,18 @@
+window.alert("Hello, World!");
+
+document.write("Hello, World! ");
+
+var A = "It's a beautiful day! ";
+document.write(A);
+
+document.write("World replied, \"Yes, it's a beautiful day!\" ");
+
+document.write("I replied, " + "\"Glad you agree!\" ");
+
+var season = "summer", place = "beach", weather = "sunny";
+var season = season.fontcolor("green"), place = place.fontcolor("blue"), weather = weather.fontcolor("yellow");
+
+document.write("It's " + weather + ". ");
+
+B = (2 + 2);
+document.write(B);
